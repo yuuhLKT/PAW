304a346350ecb076934c39ceaf84bba0f6c0ca3f
Yuri Luiz <yuri.luizkt@gmail.com>
2025-04-28 19:00:26 -0300

    Add login component with HTML, CSS, and TypeScript files. Implement login form functionality, including email and password input, and basic styling for the login interface.

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log('Login submitted:', { email: this.email, password: this.password });
+  }
+}
