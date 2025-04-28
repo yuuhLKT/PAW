a6044b5cead4dae303719d762523287d64647f91
Yuri Luiz <yuri.luizkt@gmail.com>
2025-04-28 19:00:37 -0300

    Add app container structure and styling to main component. Introduce a new CSS class for the app container to ensure full viewport height and flex layout. Update HTML to wrap the router outlet in the app container. Remove unused HttpClientModule import from the component TypeScript file.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
 }
