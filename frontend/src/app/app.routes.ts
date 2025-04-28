7d1cbc00296bf2e23400af78f08514fb7c8de5c0
Yuri Luiz <yuri.luizkt@gmail.com>
2025-04-28 19:00:30 -0300

    Update routing configuration to include login, signup, and message components. Redirect root path to login and define routes for user authentication and messaging functionalities.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MessageComponent } from './message/message.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'message', component: MessageComponent }
+];
